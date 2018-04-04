c666e652e7a5bd7e6ff85288aa8a717a308e39cf
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-04-04 09:38:27 +0400

    fix localstorage module

--- src/utils/local-storage.js
+++ src/utils/local-storage.js
@@ -2,7 +2,8 @@ const localStorageApi = window.localStorage;
 
 const localStorage = {
     getItem: (...args) => JSON.parse(localStorageApi.getItem(...args)),
-    removeItem: (...args) => JSON.stringify(localStorageApi.removeItem(...args)),
+    setItem: (key, value) => localStorageApi.setItem(key, JSON.stringify(value)),
+    removeItem: (...args) => localStorageApi.removeItem(...args),
     clear: localStorageApi.clear,
     getLength: () => localStorageApi.length
 };
