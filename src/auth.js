0aa10c4dca9495f89a117b18c2dfdab1837ce838
Eugene Vorozhtsov <eugene.vorozhtsoff@gmail.com>
2018-04-04 10:04:24 +0400

    add auth fn

---
+++ src/auth.js
@@ -0,0 +1,14 @@
+import generateId from './utils/generate-id';
+import localStorage from './utils/local-storage';
+
+const auth = () => {
+    const id = localStorage.getItem('id');
+    if (!id) {
+        localStorage.setItem('id', generateId());
+        return auth();
+    }
+
+    return id;
+};
+
+export default auth;
